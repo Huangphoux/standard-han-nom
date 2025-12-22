19fa769d46e7c6ddc83e6c792a0cf56a83bf228d
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-12-22 09:02:11 +0700

    I give up on porting Datastar

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ app, rt = fast_app(
     pico=False,
     hdrs=(
         Link(rel="icon", href="https://fav.farm/❤️"),  # favicon
-        # Vendored
-        Link(rel="stylesheet", href="simple_2.3.7.css"),
         Link(rel="stylesheet", href="style.css"),
-        Script(src="htmx.min_2.0.8.js"),
+        # Vendored
+        Link(rel="stylesheet", href="simple@2.3.7.css"),
+        Script(src="htmx@2.0.8.js"),
+        Script(src="morph@0.7.4.js"),
+        Script(src="preload@2.1.2.js"),
     ),
     htmlkw={"lang": "vi"},
     static_path="static",
@@ -33,7 +35,7 @@ with open("csv/after-processing-list.csv", encoding="utf-8") as f:
     rows = list(reader)
 
 
-@rt
+@rt("/")
 def index(search: Optional[str] = None):
     if not search:  # để trống
         result = []
@@ -47,7 +49,7 @@ def index(search: Optional[str] = None):
 
     return (
         Title("Tìm kiếm chữ Hán Nôm"),
-        Body(hx_boost="true")(
+        Body(hx_boost="true", hx_ext="morph", hx_swap="morph:innerHTML")(
             Header(
                 Nav(
                     A("Trang chủ", href="/"),
@@ -55,27 +57,33 @@ def index(search: Optional[str] = None):
                 H1("Tìm chữ Hán Nôm"),
                 P("Tìm bằng chữ Quốc Ngữ, tra kết quả ra chữ Hán Nôm"),
             ),
-            Main(
-                Form(
-                    role="search",
-                    action=index,
-                    method="get",
-                )(
-                    Label(
-                        "Tìm chữ Hán Nôm",
-                    ),
-                    Input(
-                        value=search.strip() if search else "",
-                        type="search",
-                        name="search",
-                        maxlength="140",
-                        size="40",
-                        placeholder="Gõ chữ Quốc Ngữ vào đây",
-                        required=True,
-                        autofocus=True,
-                        onfocus="let temp=this.value; this.value=''; this.value=temp",
-                    ),
-                    Input(type="submit", value="Tìm kiếm"),
+            Main(hx_ext="preload")(
+                Form(role="search", action=index, method="get", preload=True)(
+                    Fieldset(
+                        Label(
+                            "Tìm chữ Hán Nôm",
+                        ),
+                        Input(
+                            value=search.strip() if search else "",
+                            type="search",
+                            name="search",
+                            maxlength="140",
+                            size="40",
+                            placeholder="Gõ chữ Quốc Ngữ vào đây",
+                            required=True,
+                            autofocus=True,
+                            onfocus="let temp=this.value; this.value=''; this.value=temp",
+                            hx_get="/",
+                            hx_trigger="input changed delay:500ms, keyup[key=='Enter'], load",
+                            hx_target="body",
+                            hx_push_url="true",
+                        ),
+                        Input(
+                            type="submit",
+                            value="Tìm kiếm",
+                            preload=True,
+                        ),
+                    )
                 ),
                 Table(
                     Thead(
